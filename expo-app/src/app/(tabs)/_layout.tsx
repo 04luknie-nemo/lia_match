44950b55ec6e6bdf8ac3d60b6ee5c2c67edcea53
Lukas <04luknie@skola.boras.se>
2026-09-25 12:39:00 +0200

    Modified tabslayout

--- expo-app/src/app/(tabs)/_layout.tsx
+++ expo-app/src/app/(tabs)/_layout.tsx
@@ -1,11 +1,40 @@
+import Feather from "@expo/vector-icons/Feather";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="feed" options={{ title: "Feed" }} />
-      <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: (props) => <Feather name="home" {...props} />,
+        }}
+      />
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          title: "Notifications",
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="bell" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: (props) => <Feather name="settings" {...props} />,
+        }}
+      />
+      <Tabs.Screen
+        name="workplaces"
+        options={{
+          title: "Workplaces",
+          headerShown: false,
+          tabBarIcon: (props) => <Feather name="activity" {...props} />,
+        }}
+      />
     </Tabs>
   );
 }
