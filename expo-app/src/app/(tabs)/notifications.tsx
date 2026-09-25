9a81ee2270f80cb0d22166c996151006954936ec
Lukas <04luknie@skola.boras.se>
2026-09-25 12:39:29 +0200

    Added notification page

--- expo-app/src/app/(tabs)/notifications.tsx
+++ expo-app/src/app/(tabs)/notifications.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from "react-native";
 
-export default function FeedScreen() {
+export default function NotificationScreen() {
   return (
     <View>
-      <Text>Feed</Text>
+      <Text>Notifications</Text>
     </View>
   );
 }
